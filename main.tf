d4a4e1d0603c1b3f01db605de34276028eaf786d
mmartofel <mmartofe@redhat.com>
2025-03-07 23:39:43 +0100

    implement cluster_name variable

--- main.tf
+++ main.tf
@@ -1,30 +1,36 @@
 provider "aws" {
-  region = "us-east-1"           # Specify your desired AWS region
+  region = "us-west-2" # Specify your desired AWS region
 }
 
 data "aws_availability_zones" "available" {}
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+  default     = "zenek"
+}
 
 resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "eks_vpc"
+    Name = "${var.cluster_name}_vpc"
   }
 }
 
 resource "aws_subnet" "eks_subnet" {
-  count                  = 2
+  count                   = 2
   vpc_id                 = aws_vpc.eks_vpc.id
   cidr_block             = "10.0.${count.index}.0/24"
   availability_zone      = element(data.aws_availability_zones.available.names, count.index)
 
   tags = {
-    Name = "eks_subnet_${count.index}"
+    Name = "${var.cluster_name}_subnet_${count.index}"
   }
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "my-eks-cluster"
+  name     = var.cluster_name
   role_arn = aws_iam_role.eks_role.arn
   vpc_config {
     subnet_ids = aws_subnet.eks_subnet[*].id
@@ -32,7 +38,7 @@ resource "aws_eks_cluster" "eks_cluster" {
 }
 
 resource "aws_iam_role" "eks_role" {
-  name = "eks_role"
+  name = "${var.cluster_name}_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -50,7 +56,7 @@ resource "aws_iam_role" "eks_role" {
 
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "my-node-group"
+  node_group_name = "${var.cluster_name}_node_group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = aws_subnet.eks_subnet[*].id
 
@@ -62,7 +68,7 @@ resource "aws_eks_node_group" "eks_node_group" {
 }
 
 resource "aws_iam_role" "eks_node_role" {
-  name = "eks_node_role"
+  name = "${var.cluster_name}_node_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -77,3 +83,4 @@ resource "aws_iam_role" "eks_node_role" {
     ]
   })
 }
+
